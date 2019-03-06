2929200b284b4e554f0985694605ccbab629e19f
reimalice <reynaldo.anton.666gmail.com>
2019-03-05 23:08:06 -0400

    integracion del login con la pagina inicial

--- src/app/modulos/login/login.component.ts
+++ src/app/modulos/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { MatProgressBar, MatButton } from '@angular/material';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 
 @Component({
@@ -7,11 +12,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  @ViewChild(MatProgressBar) progressBar: MatProgressBar;
+  @ViewChild(MatButton) submitButton: MatButton;
 
+  signinForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.signinForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      rememberMe: new FormControl(false)
+    })
+  }
+
+  signin() {
+    const signinData = this.signinForm.value
+    console.log(signinData);
+
+    this.submitButton.disabled = true;
+    this.progressBar.mode = 'indeterminate';
   }
 
 }

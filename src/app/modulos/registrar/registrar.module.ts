296734262ee501b25d6f53b4a91d6f42a0d712f6
reimalice <reynaldo.anton.666gmail.com>
2019-03-05 23:25:51 -0400

    creacion del modulo y del componente para el registro de usuarios

---
+++ src/app/modulos/registrar/registrar.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrarComponent } from './registrar/registrar.component';
+
+@NgModule({
+  declarations: [RegistrarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegistrarModule { }

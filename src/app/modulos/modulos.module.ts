bcc3fc3f5b3208cf2b47c99983d4def2fb7a98c1
reimalice <reynaldo.anton.666gmail.com>
2019-03-05 23:38:26 -0400

    generacion del formulario de registro de nueva cuenta

--- src/app/modulos/modulos.module.ts
+++ src/app/modulos/modulos.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import {MaterialModule} from '../material.module';
 import {PrincipalComponent} from './home/principal/principal.component'
+import { RegistrarComponent } from 'src/app/modulos/registrar/registrar/registrar.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
-    PrincipalComponent],
+    PrincipalComponent,
+    RegistrarComponent],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    
   ],
   exports:[
     

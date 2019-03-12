43b60de3241d1aa601f8467f4a0c6d0b9867ca18
reimalice <reynaldo.anton.666gmail.com>
2019-03-11 23:47:20 -0400

    creacion de la vista del componete de paquete

--- src/app/modulos/app/principal/principal.routing.ts
+++ src/app/modulos/app/principal/principal.routing.ts
@@ -1,25 +1,14 @@
 import { Routes } from '@angular/router';
 
-
-
+import{PrincipalAppComponent} from './principal.component';
+import{CrearPaqueteComponent} from './crear-paquete/crear-paquete.component'
 export const PrincipalRoutes: Routes = [
   {
     path: '',
-    component: ProfileComponent,
+    component: PrincipalAppComponent,
     children: [{
-      path: 'overview',
-      component: ProfileOverviewComponent,
-      data: { title: 'Overview', breadcrumb: 'OVERVIEW' }
-    }, 
-    {
-      path: 'settings',
-      component: ProfileSettingsComponent,
-      data: { title: 'Settings', breadcrumb: 'SETTINGS' }
-    }, 
-    {
-      path: 'blank',
-      component: ProfileBlankComponent,
-      data: { title: 'Blank', breadcrumb: 'BLANK' }
+      path: 'crear-paquete',
+      component: CrearPaqueteComponent,
     }]
   }
 ];
